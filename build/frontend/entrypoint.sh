c460622d5e7363b1073be52dca1dcfcb13e43643
cohakim <cohakim@gmail.com>
2019-05-19 21:35:55 +0900

    [from now] 2019/05/19 21:35:55
    diff --git a/build/frontend/entrypoint.sh b/build/frontend/entrypoint.sh
    index bdfeaa8..b5bb8e2 100644
    --- a/build/frontend/entrypoint.sh
    +++ b/build/frontend/entrypoint.sh
    @@ -5,6 +5,5 @@ mkdir -p /app/frontend/tmp/cache
     bundle exec rails db:create
     bundle exec ridgepole -c /app/shared/config/database.yml -E $RAILS_ENV --apply -f /app/shared/db/Schemafile
     bundle exec rails assets:precompile
    -bundle exec rails webpacker:compile
    
     exec $@

--- build/frontend/entrypoint.sh
+++ build/frontend/entrypoint.sh
@@ -5,6 +5,5 @@ mkdir -p /app/frontend/tmp/cache
 bundle exec rails db:create
 bundle exec ridgepole -c /app/shared/config/database.yml -E $RAILS_ENV --apply -f /app/shared/db/Schemafile
 bundle exec rails assets:precompile
-bundle exec rails webpacker:compile
 
 exec $@
