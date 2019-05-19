bcdc42f0798b2b41352f308e3ba0b61233256748
cohakim <cohakim@gmail.com>
2019-05-19 18:43:32 +0900

    [from now] 2019/05/19 18:43:32
    diff --git a/app/backend/app/jobs/clean_job.rb b/app/backend/app/jobs/clean_job.rb
    index 2f824ba..34ac104 100644
    --- a/app/backend/app/jobs/clean_job.rb
    +++ b/app/backend/app/jobs/clean_job.rb
    @@ -1,16 +1,12 @@
     class CleanJob < ApplicationJob
    -  include Shoryuken::Worker
    -
       queue_as 'default.fifo'
    -  shoryuken_options auto_delete: true, auto_visibility_timeout: true
    
       def perform(order_id)
    -    # order = Order.find(order_id)
    -    # CollectService.call(order)
    -    # DestroyService.call(order)
    -    sleep 100
    +    order = Order.find(order_id)
    +    CollectService.call(order)
    +    DestroyService.call(order)
       rescue => ex
    +    # TODO: 例外を通知する
         order.fail!
    -    raise ex
       end
     end
    diff --git a/app/backend/app/models/concerns/.keep b/app/backend/app/models/concerns/.keep
    deleted file mode 100644
    index e69de29..0000000

--- app/backend/app/jobs/clean_job.rb
+++ app/backend/app/jobs/clean_job.rb
@@ -1,16 +1,12 @@
 class CleanJob < ApplicationJob
-  include Shoryuken::Worker
-
   queue_as 'default.fifo'
-  shoryuken_options auto_delete: true, auto_visibility_timeout: true
 
   def perform(order_id)
-    # order = Order.find(order_id)
-    # CollectService.call(order)
-    # DestroyService.call(order)
-    sleep 100
+    order = Order.find(order_id)
+    CollectService.call(order)
+    DestroyService.call(order)
   rescue => ex
+    # TODO: 例外を通知する
     order.fail!
-    raise ex
   end
 end
