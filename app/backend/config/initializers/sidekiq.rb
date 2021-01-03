df04634db0a9405feac0ed8c3a3592116f7fc840
cohakim <cohakim@gmail.com>
2021-01-04 01:42:53 +0900

    Fix processing interval

--- app/backend/config/initializers/sidekiq.rb
+++ app/backend/config/initializers/sidekiq.rb
@@ -9,7 +9,7 @@ schedule     = YAML.load_file(Rails.root.join('config', 'scheduler.yml'))
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis }
-  config.average_scheduled_poll_interval = 3
+  config.average_scheduled_poll_interval = 1
   config.on(:startup) do
     Sidekiq.schedule = schedule
     Sidekiq::Scheduler.reload_schedule!
